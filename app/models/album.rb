80b4abb9239ecc75370265f02ada4a6a46a44156
angel <apineross@unal.edu.co>
2025-05-11 13:37:13 -0500

    Add Album model with fields, relationships, and validations

---
+++ app/models/album.rb
@@ -0,0 +1,17 @@
+class Album
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  # Definir campos
+  field :title, type: String
+  field :artist, type: String
+  field :release_date, type: Date
+  field :spotify_id, type: String
+  field :cover_image, type: String
+  
+  # Relaciones (si las hay)
+  has_many :songs
+  
+  # Validaciones
+  validates :title, presence: true
+end
